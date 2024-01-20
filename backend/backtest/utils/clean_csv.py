f5913a3b8464f8f85126349441c93fc11c198bb0
Pol Bachelin <pol.bachelin@epitech.eu>
2024-01-20 09:58:12 +0100

    fixed dashboard

--- backend/backtest/utils/clean_csv.py
+++ backend/backtest/utils/clean_csv.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 
 def clean_numeric(col: str, col_replace: str):
-    return pd.to_numeric(col.str.replace(col_replace, ""), errors="coerce")
+    return pd.to_numeric(col.replace(col_replace, ""), errors="coerce")
